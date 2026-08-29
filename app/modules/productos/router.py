7511e2d31558de7b745653ed4f535e32020004db
Eze1504 <eze.sarabia04@gmail.com>
2026-08-29 16:45:33 -0300

    Agrega actualizacion y alerta de stock

--- app/modules/productos/router.py
+++ app/modules/productos/router.py
@@ -1,7 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
-from .schemas import ProductoCreate, ProductoRead
-from .service import crear_producto, listar_productos
+from .schemas import ProductoCreate, ProductoRead, ProductoUpdate
+from .service import (
+    crear_producto,
+    listar_productos,
+    actualizar_producto,
+    alerta_stock
+)
 
 
 router = APIRouter(
@@ -18,3 +23,30 @@ def crear(datos: ProductoCreate):
 @router.get("/", response_model=list[ProductoRead])
 def listar():
     return listar_productos()
+
+@router.put("/{producto_id}", response_model=ProductoRead)
+def actualizar(producto_id: int, datos: ProductoUpdate):
+    producto = actualizar_producto(producto_id, datos)
+
+    if producto is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Producto no encontrado"
+        )
+
+    return producto
+
+@router.get("/{producto_id}/alerta-stock")
+def consultar_alerta_stock(producto_id: int):
+    alerta = alerta_stock(producto_id)
+
+    if alerta is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Producto no encontrado"
+        )
+
+    return {
+        "producto_id": producto_id,
+        "alerta_stock": alerta
+    }
--- app/modules/productos/service.py
+++ app/modules/productos/service.py
@@ -1,5 +1,4 @@
-from .schemas import ProductoCreate, ProductoRead
-
+from .schemas import ProductoCreate, ProductoRead, ProductoUpdate
 
 productos: list[ProductoRead] = []
 
@@ -17,3 +16,32 @@ def crear_producto(datos: ProductoCreate) -> ProductoRead:
 
 def listar_productos() -> list[ProductoRead]:
     return productos
+
+def actualizar_producto(
+    producto_id: int,
+    datos: ProductoUpdate
+) -> ProductoRead | None:
+
+    for indice, producto in enumerate(productos):
+        if producto.id == producto_id:
+
+            datos_actualizados = producto.model_dump()
+
+            cambios = datos.model_dump(exclude_unset=True)
+
+            datos_actualizados.update(cambios)
+
+            producto_actualizado = ProductoRead(**datos_actualizados)
+
+            productos[indice] = producto_actualizado
+
+            return producto_actualizado
+
+    return None
+
+def alerta_stock(producto_id: int) -> bool | None:
+    for producto in productos:
+        if producto.id == producto_id:
+            return producto.stock < producto.stock_minimo
+
+    return None
